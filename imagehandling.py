f52c0264b68b3052776831ccaf2e104537de6b01
Shathebean <shaheen@gmail.com>
2021-02-07 23:35:05 -0800

    Made it find topography of australia

--- imagehandling.py
+++ imagehandling.py
@@ -1,34 +1,35 @@
 from PIL import Image
 import numpy as np
 import sys
-np.set_printoptions(threshold=sys.maxsize)
+# np.set_printoptions(threshold=sys.maxsize)
 
 image = Image.open("keycropped.png")
-# print(image.format)
-# print(image.size)
-# print(image.mode)
 array = np.asarray(image)
 array = array.view([(f'f{i}',array.dtype) for i in range(array.shape[-1])])[...,0].astype('O')
-# print([(f'f{i}',array.dtype) for i in range(array.shape[-1])])
-# print(array.shape)
-# print(array[1,1])
-# # print(array[:,2])
-# print(len(np.unique(array[:,2])))
 unique = list(np.unique(array[:,2]))
-
-victoria = Image.open("victoria.png")
+unique = [a[0:3] for a in unique]
+heights = [0, 1, 2, 6, 13, 25, 44, 70, 105, 151, 209, 279, 356, 466, 585, 724, 882, 1063, 1267, 1495, 1751, 2034]
+colordict = dict(zip(unique,heights))
+victoria = Image.open("victoria-min.jpg")
 vicarray = np.asarray(victoria)
-# print(vicarray.shape)
 vicarray = vicarray.view([(f'f{i}',vicarray.dtype) for i in range(vicarray.shape[-1])])[...,0].astype('O')
+count = np.count_nonzero(np.vectorize(lambda x: x in unique)(vicarray))
+def minimize_color_dist(color1, list_of_colors):
+    minimum = 255**2
+    min_color = None
+    for index, color in enumerate(list_of_colors):
+        sum = 0
+        for i,j in zip(color1, color):
+            sum += (i-j)**2
+        if sum < minimum:
+            minimum = sum
+            min_color = color
 
-ont = np.count_nonzero(np.vectorize(lambda x: x in unique)(vicarray))
-print(vicarray[50,50])
-print(unique)
-print(ont/vicarray.size)
-# np.count_nonzero(array in unique)
+    return colordict[min_color]
 
-# print(array)
+elevation_array = np.vectorize(minimize_color_dist, excluded=["list_of_colors"])(vicarray, list_of_colors=unique)
 
-# b = array.view([(f'f{i}',array.dtype) for i in range(array.shape[-1])])[...,0].astype('O')
-# print(b.shape)
-# print(b[1,1])
+# rev_colordict = {item:list(key) for key, item in colordict.items()}
+# new_img_array = np.array([[rev_colordict[j] for j in i] for i in elevation_array])
+# j = Image.fromarray(np.uint8(new_img_array), "RGB")
+# j.show()
